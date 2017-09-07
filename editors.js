69df93400c6bfbaabfb98ea03be73377e81e3604
Sascha Goldhofer <post@saschagoldhofer.de>
2017-09-07 08:57:29 +0200

    Fix order of editors

--- editors.js
+++ editors.js
@@ -1,6 +1,6 @@
 module.exports = [
+    require("./editors/oneofeditor"), // must come before other editors (may contain any type)
     require("./editors/arrayeditor"),
     require("./editors/objecteditor"),
-    require("./editors/oneofeditor"),
     require("./editors/valueeditor")
 ];
