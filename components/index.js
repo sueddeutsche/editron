1e00333e8814a9ca4fa146668b671378f2a41ac6
Sascha Goldhofer <post@saschagoldhofer.de>
2017-11-19 16:24:53 +0100

    Add module exports

---
+++ components/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    container: require("./container"),
+    containerdescription: require("./containerdescription"),
+    containererrors: require("./containererrors"),
+    containerheader: require("./containerheader"),
+    overlay: require("./overlay"),
+    // overlayjson: require("./overlayjson"),
+    overlayselecttiles: require("./overlayselecttiles")
+};
---
+++ editors/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    AbstractValueEditor: require("./AbstractValueEditor"),
+    ArrayEditor: require("./arrayeditor"),
+    ObjectEditor: require("./objecteditor"),
+    OneOfEditor: require("./oneofeditor"),
+    ValueEditor: require("./valueeditor")
+};
