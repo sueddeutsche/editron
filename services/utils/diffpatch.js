f0eea9ff7b617fd3ff670f239a0305b078d51c96
Sascha Goldhofer <post@saschagoldhofer.de>
2019-11-21 16:30:02 +0100

    Cleanup

--- services/utils/diffpatch.js
+++ services/utils/diffpatch.js
@@ -4,7 +4,7 @@ const diff_match_patch = require("diff_match_patch");
 const options = {
     // used to match objects when diffing arrays, by default only === operator is used
     // this function is used only to when objects are not equal by ref
-    objectHash: (obj) => obj._id,
+    objectHash: obj => obj._id,
     arrays: {
         // default true, detect items moved inside the array (otherwise they will be registered as remove+add)
         detectMove: true,
--- webpack.serve.js
+++ webpack.serve.js
@@ -1,7 +1,24 @@
 const path = require("path");
 const config = require("./webpack.build");
 
+config.plugins = []; // reset
 config.entry["index.html"] = path.join(__dirname, "test", "support", "local-setup.html");
-config.plugins = [];
+config.entry["editron.css"] = path.join(__dirname, "editron.scss");
+config.module.rules.push({
+    test: [/wysiwyg-editor.scss/, /editron.scss$/],
+    use: [
+        "file-loader?name=[name].css",
+        "extract-loader",
+        "css-loader",
+        {
+            loader: "sass-loader",
+            options: {
+                sassOptions: {
+                    includePaths: [path.join(__dirname, "node_modules")]
+                }
+            }
+        }
+    ]
+});
 
 module.exports = config;
