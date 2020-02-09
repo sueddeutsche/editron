8cb72aad4653a2d564bb45d4bf18e45734c4d55d
Sascha Goldhofer <post@saschagoldhofer.de>
2020-02-09 12:03:26 +0100

    Fix type

--- editron.d.ts
+++ editron.d.ts
@@ -31,7 +31,7 @@ type DataServiceEvent = {
 type DataServiceObserveCallback = (DataServiceEvent) => void;
 
 // {type: "error", message: "err-msg", data: { pointer }}
-type Validator = (core: Controller, schema: JSONSchema, value: JSONObject, pointer: string) => validationResult|Promise<validationResult>;
+type Validator = (core: Controller, schema: JSONSchema, value: any, pointer: string) => validationResult|Promise<validationResult>;
 
 
 declare class JSONLibCore {
