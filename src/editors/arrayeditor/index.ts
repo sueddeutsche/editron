963fa9a7e51d94aa480363006341ee70da6fca69
Sascha Goldhofer <sascha.goldhofer@sueddeutsche.de>
2020-08-17 22:22:32 +0200

    Cleanup

--- src/editors/arrayeditor/index.ts
+++ src/editors/arrayeditor/index.ts
@@ -12,23 +12,26 @@ import { Patch } from "../../services/utils/createDiff";
 
 export type Controls = {
     add?: boolean;
-    remove?: boolean;
-    move?: boolean;
     clone?: boolean;
+    disabled: boolean;
     insert?: boolean;
     maxItems?: number;
     minItems?: number;
+    move?: boolean;
+    remove?: boolean;
+    showIndex: boolean;
 }
 
 export type ViewModel = {
-    pointer: string;
     attrs: object;
+    controls: Controls;
+    disabled: boolean;
     errors: Array<ValidationError>;
-    onAdd: Function;
     length: number;
     maxItems: number;
     minItems: number;
-    controls: Controls;
+    onadd?: Function;
+    pointer: string;
 }
 
 export type Options = {
@@ -38,12 +41,13 @@ export type Options = {
 
 
 export default class ArrayEditor extends AbstractEditor {
-    viewModel;
-    controller: Controller;
-    pointer: string;
-    children: Array<ArrayItemWrapper> = [];
     $items: HTMLElement;
+    children: Array<ArrayItemWrapper> = [];
+    controller: Controller;
     onAdd: Function;
+    pointer: JSONPointer;
+    viewModel: ViewModel;
+
 
     static editorOf(pointer: JSONPointer, controller: Controller) {
         const schema = controller.service("schema").get(pointer);
@@ -62,18 +66,20 @@ export default class ArrayEditor extends AbstractEditor {
         const schema = this.getSchema();
         const data = this.getData();
         this.viewModel = {
-            pointer,
             attrs: {},
+            disabled: false,
             errors: controller.service("validation").getErrorsAndWarnings(pointer),
-            onadd: this.onAdd,
             length: data.length,
             maxItems: schema.maxItems || Infinity,
             minItems: schema.minItems || 0,
+            onadd: this.onAdd,
+            pointer,
             ...options,
 
             controls: {
                 add: false,
                 clone: true,
+                disabled: false,
                 remove: true,
                 move: true,
                 insert: true,
@@ -125,40 +131,40 @@ export default class ArrayEditor extends AbstractEditor {
     }
 
     applyPatches(patch: Patch): void {
+        const { pointer, controller, viewModel, children, $items } = this;
+
         // fetch a copy of the original list
-        const originalChildren = Array.from(this.children);
+        const originalChildren = Array.from(children);
         // and patch the current list
-        diffpatch.patch(this.children, patch);
+        diffpatch.patch(children, patch);
 
         // search for inserted children
-        this.children.forEach((child, index) => {
+        children.forEach((child, index) => {
             if (child instanceof ArrayItemWrapper === false) {
-                const pointer = `${this.pointer}/${index}`;
-                const newChild = new ArrayItemWrapper(pointer, this.controller, this.viewModel.controls);
-                // @insert?
-                this.children[index] = newChild;
+                const newChild = new ArrayItemWrapper(`${pointer}/${index}`, controller, viewModel.controls);
+                children[index] = newChild;
             }
         });
 
         // search for removed children (item wrappers)
         originalChildren.forEach(child => {
-            if (this.children.indexOf(child) === -1) {
+            if (children.indexOf(child) === -1) {
                 child.destroy();
             }
         });
 
         // update view: move and inserts nodes
-        const currentLocation = this.controller.service("location").getCurrent();
+        const currentLocation = controller.service("location").getCurrent();
         const changePointer = {};
 
-        for (let i = 0, l = this.children.length; i < l; i += 1) {
-            const previousPointer = this.children[i].getPointer();
-            const currentPointer = `${this.pointer}/${i}`;
+        for (let i = 0, l = children.length; i < l; i += 1) {
+            const previousPointer = children[i].getPointer();
+            const currentPointer = `${pointer}/${i}`;
 
             // update current location
             if (currentLocation.indexOf(previousPointer) === 0) {
                 const editorLocation = currentLocation.replace(previousPointer, currentPointer);
-                this.controller.service("location").setCurrent(editorLocation);
+                controller.service("location").setCurrent(editorLocation);
             }
 
             // update child views to match patched list
@@ -167,45 +173,41 @@ export default class ArrayEditor extends AbstractEditor {
             }
 
             // this updates the array-item wrapper
-            this.children[i].updatePointer(currentPointer);
+            children[i].updatePointer(currentPointer);
 
-            if (this.$items.children[i] === this.children[i].toElement()) {
+            if ($items.children[i] === children[i].toElement()) {
                 // skip moving node, already in place
                 continue;
-            } else if (i + 1 < this.$items.children.length) {
+            } else if (i + 1 < $items.children.length) {
                 // move node to correct position
-                this.$items.insertBefore(this.children[i].toElement(), this.$items.children[i + 1]);
+                $items.insertBefore(children[i].toElement(), $items.children[i + 1]);
             } else {
                 // remaining nodes may be simply appended
-                this.$items.appendChild(this.children[i].toElement());
+                $items.appendChild(children[i].toElement());
             }
         }
     }
 
     rebuildChildren(): void {
+        const { pointer, controller, viewModel, children, $items } = this;
         const data = this.getData();
-        // delete all child editors
-        this.children.forEach(editor => editor.destroy());
-        this.children.length = 0;
-        this.$items.innerHTML = "";
+        children.forEach((editor: ArrayItemWrapper) => editor.destroy());
+        children.length = 0;
+        $items.innerHTML = "";
         // recreate child editors
         data.forEach((item, index) => {
-            const childPointer = `${this.pointer}/${index}`;
-            const childEditor = new ArrayItemWrapper(childPointer, this.controller, this.viewModel.controls);
-            this.$items.appendChild(childEditor.toElement());
-            this.children.push(childEditor);
+            const childEditor = new ArrayItemWrapper(`${pointer}/${index}`, controller, viewModel.controls);
+            $items.appendChild(childEditor.toElement());
+            children.push(childEditor);
         });
     }
 
     updateControls(): void {
-        this.viewModel.length = this.children.length;
-        this.viewModel.onadd = this.viewModel.maxItems > this.children.length ? this.onAdd : false;
-        this.dom.classList.toggle("has-add-disabled", this.viewModel.maxItems <= this.children.length);
-        this.dom.classList.toggle("has-remove-disabled", this.viewModel.minItems >= this.children.length);
-    }
-
-    getPointer(): JSONPointer {
-        return this.pointer;
+        const { dom, viewModel, children } = this;
+        viewModel.length = children.length;
+        viewModel.onadd = viewModel.maxItems > children.length ? this.onAdd : undefined;
+        dom.classList.toggle("has-add-disabled", viewModel.maxItems <= children.length);
+        dom.classList.toggle("has-remove-disabled", viewModel.minItems >= children.length);
     }
 
     render(): void {
@@ -216,8 +218,7 @@ export default class ArrayEditor extends AbstractEditor {
         if (this.viewModel == null) {
             return;
         }
-
-        this.children.forEach(editor => editor.destroy());
+        this.children.forEach((editor: ArrayItemWrapper) => editor.destroy());
         this.viewModel = null;
         m.render(this.dom, m("i"));
     }
