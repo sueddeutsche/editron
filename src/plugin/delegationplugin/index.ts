ff1aab98ce02cdafa155f3dffa7de89c81b3fc35
Sascha Goldhofer <sascha.goldhofer@sueddeutsche.de>
2020-08-20 11:22:42 +0200

    Update delegation pugin

--- src/plugin/delegationplugin/index.ts
+++ src/plugin/delegationplugin/index.ts
@@ -1,17 +1,22 @@
 import m from "mithril";
 import Controller from "../../Controller";
-import AbstractEditor from "../../editors/AbstractEditor";
+import AbstractEditor, { Options as EditorOptions } from "../../editors/AbstractEditor";
 import { JSONPointer } from "../../types";
 import { Editor } from "../../editors/Editor";
 import { Button } from "mithril-material-forms/index";
 import { Plugin } from "../index";
+import OverlayService from "../../services/OverlayService";
+import { translate } from "../../utils/i18n";
 
 
 class AbstractDelegationEditor extends AbstractEditor {
 
+    options: EditronSchemaOptions & EditorOptions;
+
     static editorOf(pointer: JSONPointer, controller: Controller, options) {
         return options.delegate != null && options.isDelegated !== true;
     }
+
 
     constructor(pointer: JSONPointer, controller: Controller, options) {
         options.isDelegated = true;
@@ -23,21 +28,49 @@ class AbstractDelegationEditor extends AbstractEditor {
         console.log("update delegation pliugin", event);
     }
 
-    delegate(pointer: JSONPointer) { // eslint-disable-line @typescript-eslint/no-unused-vars
+    delegate(pointer: JSONPointer, options) { // eslint-disable-line @typescript-eslint/no-unused-vars
         throw new Error("notify has not been implemented in 'DelegationEditor'");
     }
 
     render() {
         m.render(this.dom, m(`.editron-delegate.editron-delegate--${this.getSchema().type}`,
-            m(Button, { onclick: () => this.delegate(this.pointer) }, "edit")
+            m(Button,
+                { onclick: () => this.delegate(this.pointer, this.options) },
+                translate(this.options.delegate?.buttonText || "open")
+            )
         ));
+    }
+
+    destroy() {
+        m.render(this.dom, m("o"));
     }
 }
 
 
+export type DelegationEvent = {
+    pointer: JSONPointer;
+    controller: Controller;
+    editor: Editor;
+}
+
+export enum DelegationTarget {
+    Overlay = "overlay"
+}
+
+
+/** settings in editron:ui config */
+export type EditronSchemaOptions = {
+    delegate?: {
+        /** set to "overlay", to open editor in overlay-panel */
+        delegateTo?: DelegationTarget;
+        /** button text for inline delegation. Defaults to "open" */
+        buttonText?: string;
+    }
+}
+
 
 export type Options = {
-    onDelegation: ({ pointer: JSONPointer, controller: Controller, editor }) => void;
+    onDelegation?: (event: DelegationEvent) => void;
 }
 
 
@@ -47,10 +80,11 @@ export default class DelegationPlugin {
     dom: HTMLElement;
     current: Editor;
     controller: Controller;
-    onDelegation: Options["onDelegation"];
+    onDelegation?: (event: DelegationEvent) => void;
 
-    constructor(options: Options) {
-        this.onDelegation = options.onDelegation;
+
+    constructor(options: Options = {}) {
+        this.onDelegation = options.onDelegation || this.onDelegation;
         this.dom = document.createElement("div");
     }
 
@@ -64,17 +98,30 @@ export default class DelegationPlugin {
         return this;
     }
 
-    delegate(pointer: JSONPointer) {
+    delegateToOverlay(editor: Editor): Promise<void> {
+        return OverlayService
+            .open(editor.getElement(), { abortButton: false })
+            .then(() => this.controller.destroyEditor(editor));
+    }
+
+    delegate(pointer: JSONPointer, options: EditronSchemaOptions) {
         if (this.current) {
-            this.current.destroy();
+            this.controller.destroyEditor(this.current);
         }
 
         this.current = this.controller.createEditor(pointer, this.dom, { isDelegated: true });
 
-        this.onDelegation({
-            controller: this.controller,
-            pointer,
-            editor: this.current
-        });
+        if (options?.delegate?.delegateTo === DelegationTarget.Overlay) {
+            this.delegateToOverlay(this.current);
+            return;
+        }
+
+        if (this.onDelegation) {
+            this.onDelegation({
+                controller: this.controller,
+                pointer,
+                editor: this.current
+            });
+        }
     }
 }
