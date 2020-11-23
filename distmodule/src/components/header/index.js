d862f190a7f8696a02ddcc547b997faeaee5be9f
Sascha Goldhofer <sascha.goldhofer@sueddeutsche.de>
2020-11-23 10:03:57 +0100

    Update dist

--- distmodule/src/components/header/index.js
+++ distmodule/src/components/header/index.js
@@ -3,9 +3,9 @@ import populated from "../../utils/populated";
 import { renderAction } from "../actions";
 export const CHILD_CONTAINER_SELECTOR = ".ed-header__children";
 function getClass(hasAction, { title, icon, disabled }) {
-    let classname = `${title ? "with-title" : "no-title"}`;
+    let classname = `${title !== "" ? "with-title" : "no-title"}`;
     classname += ` ${hasAction ? "with-actions" : "no-actions"}`;
-    classname += ` ${icon ? "with-icon" : "no-icon"}`;
+    classname += ` ${icon !== "" ? "with-icon" : "no-icon"}`;
     classname += disabled ? " is-disabled" : "";
     return classname;
 }
@@ -29,8 +29,9 @@ export default {
         };
         const { actions } = attrs;
         const hasAction = (actions === null || actions === void 0 ? void 0 : actions.length) > 0;
+        const withTitle = hasAction || attrs.title !== "" || attrs.icon !== "";
         return m(".ed-header", {
             "class": getClass(hasAction, attrs)
-        }, m(".ed-header__title", populated(vnode.attrs.icon, m("i.mmf-icon", attrs.icon)), (!attrs.hideTitle) && m("h2", attrs.title), vnode.children), vnode.attrs.headerContent && m(CHILD_CONTAINER_SELECTOR), (actions === null || actions === void 0 ? void 0 : actions.length) > 0 && m(".ed-header__actions", actions.map(action => renderAction(action))));
+        }, withTitle && m(".ed-header__title", populated(vnode.attrs.icon, m("i.mmf-icon", attrs.icon)), (!attrs.hideTitle) && m("h2", attrs.title), vnode.children), vnode.attrs.headerContent && m(CHILD_CONTAINER_SELECTOR), (actions === null || actions === void 0 ? void 0 : actions.length) > 0 && m(".ed-header__actions", actions.map(action => renderAction(action))));
     }
 };
